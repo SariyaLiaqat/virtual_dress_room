66b20cccf1e16be23306de0a2e50379b46ca005e
SariyaLiaqat <sariyaliaqat3@gmail.com>
2025-09-03 00:54:44 +0500

    Added project structure and placeholder screens with routing

---
+++ lib/screens/avatar_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AvatarScreen extends StatelessWidget {
+  const AvatarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Avatar")),
+      body: const Center(child: Text("Avatar Screen")),
+    );
+  }
+}
